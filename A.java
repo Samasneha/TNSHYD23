6788d89c96f95ec73ee45411c87decac2938a008
Samasneha <samasneha123@gmail.com>
2024-08-17 23:23:05 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+package userdefined;
+
+public class A {
+	public void display() {
+		System.out.println("userdefined");
+	}
+
+}
---
+++ C.java
@@ -0,0 +1,13 @@
+package userdefined;
+
+public class C {
+	private void display() {
+		System.out.println("welcome to hyderabad");
+	}
+	public static void main(String[]args) {
+		C obj = new C();
+		obj.display();
+		
+	}
+
+}
